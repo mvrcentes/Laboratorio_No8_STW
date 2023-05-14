9e0f983d0b1d48fa8c15f89780b8e881af412229
Marco Ramirez <mvrcentes@hotmail.com>
2023-05-14 14:38:40 -0600

    Add configurations

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,24 +1,91 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
 
+import { useConfigContext } from "../../Contexts/Config"
+
 import { Dropdown } from "../Dropdown/Dropdown"
 
+import styles from "./Form.module.css"
+
 export const Form = () => {
-    const [selectedCharacter, setSelectedCharacter] = useState(null)
+    const { config, setConfigValue } = useConfigContext()
+
+    const onChangeLenght = (e) => {
+        const value = parseInt(e.target.value)
+        if (value >= 4 && value <= 100) {
+            setConfigValue("largo", value)
+        }
+    }
+
+    const onChangeWidth = (e) => {
+        const value = parseInt(e.target.value)
+        if (value >= 4 && value <= 100) {
+            setConfigValue("ancho", value)
+        }
+    }
+
     return (
-        <Dropdown
-            name="Skin personaje"
-            value={selectedCharacter}
-            items={["sapo", "pato", "nave"]}
-            selected={selectedCharacter}
-            setSelected={setSelectedCharacter}
-        />
+        <div className={styles.Form}>
+            <Dropdown
+                name="Skin personaje"
+                value={config.character}
+                items={["sapo", "pato", "nave"]}
+                selected={config.character}
+                setSelected={(selected) =>
+                    setConfigValue("character", selected)
+                }
+            />
+
+            <Dropdown
+                name="Skin pared"
+                value={config.wall}
+                items={["tierra", "trampa", "asteroides"]}
+                selected={config.wall}
+                setSelected={(selected) =>
+                    setConfigValue("wall", selected)
+                }
+            />
+
+            <Dropdown
+                name="Skin camino"
+                value={config.path}
+                items={["grama", "agua", "espacio"]}
+                selected={config.path}
+                setSelected={(selected) =>
+                    setConfigValue("path", selected)
+                }
+            />
+
+            <div className={styles.inputs}>
+                <div className={styles.largo}>
+                    Largo
+                    <input
+                        type="number"
+                        placeholder="Largo"
+                        value={config.largo}
+                        onChange={onChangeLenght}
+                        className={styles.input}
+                    />
+                </div>
+
+                <div className={styles.ancho}>
+                    Ancho
+                    <input
+                        type="number"
+                        placeholder="Ancho"
+                        value={config.ancho}
+                        onChange={onChangeWidth}
+                        className={styles.input}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
 Form.propTypes = {
     /**
-     * 
+     *
      */
     backgroundImage: PropTypes.oneOf(["tierra", "trampa", "asteroides"]),
 }
--- src/components/Form/Form.stories.js
+++ src/components/Form/Form.stories.js
@@ -1,44 +1,15 @@
 import { Form } from "./Form"
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
     title: "Example/Form",
     component: Form,
     tags: ["autodocs"],
     argTypes: {
-        backgroundColor: { control: "color" },
-        // color: { control: "color" },
     },
-    
 }
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = {
     args: {
         label: "Form",
     },
 }
-
-export const Tierra = {
-    args: {
-        label: "Form",
-        backgroundImage: "tierra",
-    },
-}
-
-export const Trampa = {
-    args: {
-        label: "Form",
-        backgroundImage: "trampa",
-    },
-}
-
-export const Asteroides = {
-    args: {
-        label: "Form",
-        backgroundImage: "asteroides",
-    },
-}
-
-
-
