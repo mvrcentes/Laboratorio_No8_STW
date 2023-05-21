66e2edc3de85769ba7e0bea88c42aeb5ec1abdfd
Marco Ramirez <mvrcentes@hotmail.com>
2023-05-21 11:25:06 -0600

    Add link to principal page

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 import styles from "./Modal.module.css"
 
@@ -10,11 +11,16 @@ export const Modal = ({ nombre, modal, onClick }) => {
                     <div className={styles.overlay}></div>
                     <div className={styles.modal__content}>
                         <h2>{nombre}</h2>
-                        <button className={styles.close__modal} onClick={onClick}>Reiniciar</button>
+                        <Link to="/">
+                            <button
+                                className={styles.close__modal}
+                                onClick={onClick}>
+                                Reiniciar
+                            </button>
+                        </Link>
                     </div>
                 </div>
             )}
         </>
     )
 }
-
